6a1241376fd6f28560f764234a236bfc26cc17ea
thomas <thomas91@live.no>
2016-07-05 13:48:11 +0200

    Fixed naming issue.

--- dronemaster/start_edge_detector.sh
+++ dronemaster/start_edge_detector.sh
@@ -2,11 +2,11 @@
 # Starts 5 versions of the Edge Detector Node
 
 x-terminal-emulator -e '/bin/bash /home/ascend/scripts/dronemaster/edge_detector_front'
-sleep 5
+sleep 2
 x-terminal-emulator -e '/bin/bash /home/ascend/scripts/dronemaster/edge_detector_left'
-sleep 5
+sleep 2
 x-terminal-emulator -e '/bin/bash /home/ascend/scripts/dronemaster/edge_detector_right'
-sleep 5
+sleep 2
 x-terminal-emulator -e '/bin/bash /home/ascend/scripts/dronemaster/edge_detector_back'
-sleep 5
+sleep 2
 $SHELL
