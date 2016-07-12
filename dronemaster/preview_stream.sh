62bbf68508b4bfe11cfc4cfee5408662d0dd5773
Matias Christensen <mattivc@gmail.com>
2016-07-12 15:22:45 +0200

    Updated streming scripts

--- dronemaster/preview_stream.sh
+++ dronemaster/preview_stream.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-FRONT_PORT=12000
-LEFT_PORT=12001
-BACK_PORT=12002
-RIGHT_PORT=12003
-FISHEYE_PORT=12004
+FRONT_PORT=14000
+LEFT_PORT=14001
+BACK_PORT=14002
+RIGHT_PORT=14003
+FISHEYE_PORT=14004
 
 # Preview all streams
 gst-launch-1.0 udpsrc port=$FISHEYE_PORT ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink sync=false
