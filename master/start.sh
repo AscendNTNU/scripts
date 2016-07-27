edbd69ae785c4c22e488e4ce71d21ae47fa05734
Simen H <simenh.jobb@gmail.com>
2016-07-27 14:44:17 +0200

    Longer delays. drone/stop_everything.sh -> drone/stop.sh

--- master/start.sh
+++ master/start.sh
@@ -1,7 +1,7 @@
 clear
 
 # Start one terminal with two tabs
-echo "Opening a terminal to setup drone"
+echo "Opening a terminal to setup drone..."
 echo "  Tab 1: mavros_node"
 echo "         line_counter_nod"
 echo "         state_estimation_node"
@@ -9,7 +9,7 @@ echo "         position_controller"
 echo "         urg_node"
 echo "         lidar_obstacle_detection"
 echo "  Tab 2: gstreamer"
-sleep 1
+sleep 3
 
 gnome-terminal --tab --title="MAV" -e                                                            \
                "ssh ascend@ascend-nuc 'bash -s' < /home/ascend/dronemaster/scripts/start_mav.sh" \
@@ -18,11 +18,11 @@ gnome-terminal --tab --title="MAV" -e                                           
 
 # Start another terminal with three tabs
 echo " "
-echo "Opening a terminal with dronemaster stuff"
+echo "Opening a terminal with dronemaster stuff..."
 echo "  Tab 1: mission_debugger"
 echo "  Tab 2: start_lidar_dronemaster"
 echo "  Tab 3: start_ground_bot"
-sleep 1
+sleep 3
 
 export ROS_MASTER_URI=http://192.168.1.151:11311
 gnome-terminal --tab --title="Mission debugger" -e \
